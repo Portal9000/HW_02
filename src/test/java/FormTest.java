1f9a65bcdd58a4bb6a6fc42e3bec8f0f73382140
Sasha Zin <zinalbo@tut.by>
2023-06-14 17:38:06 +0300

    7.3-1406-fillForm

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,9 +1,10 @@
 import org.example.driver.DriverManager;
+import org.example.models.UserData;
 import org.example.steps.RegistrationFormSteps;
+import org.example.utils.JsonReader;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class FormTest extends BaseTest {
@@ -11,26 +12,25 @@ public class FormTest extends BaseTest {
     protected WebDriver driver;
     private RegistrationFormSteps registrationFormSteps;
 
-    @DataProvider(name = "data-provider")
-    public Object[][] dpMethod() {
-        return new Object[][]{{"Roman"}, {"Andrew"}};
-    }
-
     @BeforeClass
     public void preparationForTest() {
         driver = DriverManager.getDriver();
         registrationFormSteps = new RegistrationFormSteps(driver);
     }
 
-    @Test(dataProvider = "data-provider")
-    public void checkRegistrationFormData(String name) {
+    @Test(dataProvider = "userData", dataProviderClass = JsonReader.class)
+    public void checkRegistrationFormData(UserData userData) {
+        registrationFormSteps.fillForm(userData);
+        Assert.assertTrue(registrationFormSteps.getUserDataText().contains("Ivanov"));
+    }
 
-        registrationFormSteps.enterFirstName(name);
-        registrationFormSteps.enterLastName("Ivanov");
+    @Test(dataProvider = "userData", dataProviderClass = JsonReader.class)
+    public void checkRegistrationFormSeparateData(UserData userData) {
+        registrationFormSteps.enterFirstName(userData.getFirstName());
+        registrationFormSteps.enterLastName(userData.getLastName());
         registrationFormSteps.clickMaleRadioButton();
-        registrationFormSteps.enterMobileNumber("1111111111");
+        registrationFormSteps.enterMobileNumber(userData.getMobileNumber());
         registrationFormSteps.clickSubmitButton();
-
         Assert.assertTrue(registrationFormSteps.getUserDataText().contains("Ivanov"));
     }
 }
